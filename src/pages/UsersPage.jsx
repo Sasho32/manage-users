545d5b03d28df199976121823db1a627e382a424
Sasho Aleksandrov <sashosasho32@gmail.com>
2022-08-24 13:49:35 +0300

    users page basic sceleton

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,9 +1,34 @@
+import { useState, useEffect } from 'react';
+import { FadeLoader } from 'react-spinners';
 import Nav from '../layouts/Nav';
 
-function AddUserPage() {
-    document.title = 'Add user';
+function UsersPage() {
+    document.title = 'Users';
 
-    return <Nav />;
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/users/')
+            .then(res => res.json())
+            .then(({ userList }) => {
+                setUsers(userList);
+                setLoading(false);
+            })
+            .catch(e => alert('to add error page'));
+    }, []);
+
+    return loading ? (
+        <FadeLoader />
+    ) : (
+        <>
+            <Nav />
+            {!users.length && <h1>No users presented!</h1>}
+            {users.map(user => (
+                <h1 key={user.id}>{user.username}</h1>
+            ))}
+        </>
+    );
 }
 
-export default AddUserPage;
+export default UsersPage;
